93a99c4b5fe9bbfe584254c07ea0e5e02fb60cf5
Miłosz Osiński <mylosz@yahoo.pl>
2013-03-08 00:46:05 +0100

    pierwszy push

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,112 @@
+class UsersController < ApplicationController
+  # GET /users
+  # GET /users.json
+  def index
+   if current_user
+       if (current_user.username == 'Administrator')
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+	else
+  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+  	end
+    else
+        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  if current_user
+    @user = User.find(params[:id])
+       if (current_user.username == 'Administrator')||(current_user.username == @user.username)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+	else
+  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+  	end
+    else
+        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    if current_user
+    redirect_to root_url, :notice => 'Informacja! Wyloguj si&#281; aby dokona&#263; rejestracji'
+   else
+    redirect_to root_url, :notice => 'Informacja! Rejestracja zosta&#322;a wy&#322;&#261;czona'
+   end
+  end
+
+  # GET /users/1/edit
+  def edit
+    if current_user
+    @user = User.find(params[:id])
+       if (current_user.username == 'Administrator')||(current_user.username == @user.username)
+	else
+  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+  	end
+    else
+        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+
+  # POST /users
+  # POST /users.json
+def create
+  redirect_to root_url, :notice => 'Informacja! Rejestracja zosta&#322;a wy&#322;&#261;czona'
+end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+   if current_user
+    @user = User.find(params[:id])
+       if (current_user.username == 'Administrator')||(current_user.username == @user.username)
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+	else
+  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+  	end
+    else
+        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+if current_user
+    @user = User.find(params[:id])
+       if (current_user.username == 'Administrator')||(current_user.username == @user.username)
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+	else
+  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+  	end
+    else
+        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osp::Application.config.secret_token = '644c5773434be33cd4cd18bdc9416b3aaf8ac706dd9948c4f73b0dde99b6cb51ab86a52af5755ca658e4010bca8f60353984fb8d02524d80ec15595a05b6d93d'
