6442fe2b203201c5410ee76b7d6b893cd9f50afc
Miłosz Osiński <mylosz@yahoo.pl>
2013-03-18 22:10:04 +0100

    poprawienie logowania

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,8 +6,8 @@ def new
   if current_user
 	redirect_to users_path, :notice => 'Informacja! Jeste&#347; ju&#380; zalogowany!'
   else
-	@user_session = UserSession.new
         @zdjecia_stopka = Image.last(3)
+	@user_session = UserSession.new
  
 	respond_to do |format|
 		format.html # new.html.erb
@@ -19,6 +19,7 @@ end
 # POST /user_sessions
 # POST /user_sessions.xml
 def create
+  @zdjecia_stopka = Image.last(3)
   @user_session = UserSession.new(params[:user_session])
  
 	respond_to do |format|
@@ -26,8 +27,8 @@ def create
 	   format.html { redirect_to(root_url, :notice => "Informacja! U&#380;ytkownik zalogowany") }
 	   format.xml { render :xml => @user_session, :status => :created, :location => @user_session }
 	 else
-	   format.html { render :action => "new" }
-	   format.xml { render :xml => @user_session.errors, :status => :unprocessable_entity }
+	   format.html { render :action => "new", :notice => "Uwaga! U&#380;ytkownik nie zalogowany" }
+	   format.xml { render :xml => @user_session.errors, :status => :unprocessable_entity, :location => @user_session }
 	 end
 	end
 end
