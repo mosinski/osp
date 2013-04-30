9e1431f6345b1aff4861cc2d1c2dfd77a64d59c0
Miłosz Osiński <mylosz@yahoo.pl>
2013-04-30 14:37:37 +0200

    bardziej rails-owo + poprawienie błędu fancybox na PL

--- app/assets/javascripts/jquery.fancybox.js
+++ app/assets/javascripts/jquery.fancybox.js
@@ -138,7 +138,7 @@
 				wrap     : '<div class="fancybox-wrap" tabIndex="-1"><div class="fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner"></div></div></div></div>',
 				image    : '<img class="fancybox-image" src="{href}" alt="" />',
 				iframe   : '<iframe id="fancybox-frame{rnd}" name="fancybox-frame{rnd}" class="fancybox-iframe" frameborder="0" vspace="0" hspace="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen' + (IE ? ' allowtransparency="true"' : '') + '></iframe>',
-				error    : '<p class="fancybox-error">The requested content cannot be loaded.<br/>Please try again later.</p>',
+				error    : '<p class="fancybox-error">Obrazek nie może być wyświetlony, ponieważ zawiera błędy.<br/>Spróbuj ponownie za chwilę.</p>',
 				closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"></a>',
 				next     : '<a title="Następne" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
 				prev     : '<a title="Poprzednie" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
