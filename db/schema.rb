589e7b774460e8262d17ae2ba994a1597d08907b
Miłosz Osiński <mylosz@yahoo.pl>
2013-07-28 11:32:32 +0200

    poprawka

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702104624) do
+ActiveRecord::Schema.define(:version => 20130728112553) do
 
   create_table "albums", :force => true do |t|
     t.string   "tytul"
@@ -50,6 +50,23 @@ ActiveRecord::Schema.define(:version => 20130702104624) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "redactor_assets", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "data_file_name",                  :null => false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    :limit => 30
+    t.string   "type",              :limit => 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
+
+  add_index "redactor_assets", ["assetable_type", "assetable_id"], :name => "idx_redactor_assetable"
+  add_index "redactor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_redactor_assetable_type"
 
   create_table "statistics", :force => true do |t|
     t.integer  "rok"
