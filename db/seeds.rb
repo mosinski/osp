855ee5bfc5e1ff3e6ba0191533df835bb027519a
Miłosz Osiński <mylosz@yahoo.pl>
2014-01-10 12:16:28 +0100

    dodanie seed usera dla localhost-a do pracy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+case Rails.env
+
+when "development"
+puts "********Seeduje Dane na localhost************"
+User.create(:username => "localAdministrator", :password => "password", :password_confirmation => "password", :email => "user@example.pl")
+end
