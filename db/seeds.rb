1c6dd903c2d0270fc05fb88ea6736c5b211ad315
Miłosz Osiński <mylosz@yahoo.pl>
2014-01-11 13:24:20 +0100

    modyfikacja administratora lokalnego

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ case Rails.env
 
 when "development"
 puts "********Seeduje Dane na localhost************"
-User.create(:username => "localAdministrator", :password => "password", :password_confirmation => "password", :email => "user@example.pl")
+User.create(:username => "Administrator", :password => "password", :password_confirmation => "password", :email => "user@example.pl")
 end
