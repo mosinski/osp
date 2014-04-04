f864911dc0613f5d80d5c2a555dba32a5d188c8b
Miłosz Osiński <mylosz@yahoo.pl>
2014-04-05 01:28:24 +0200

    reorganizacja kodu dodanie partiali do layout-u

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path)||current_page?("/aktualnosci_grupuj")||current_page?("/imprezy")||current_page?("/interwencje")||current_page?("/inne")||current_page?("/szkolenia")||current_page?("/galeria_wszystkie") ? 'selected' : ''
+
+    content_tag(:li, :class => class_name, "onclick" => "location.href = '#{link_path}'") do
+      link_to link_text, link_path
+    end
+  end
 end
