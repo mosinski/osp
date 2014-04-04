8bc54ca35787557c7c9ff4ff7198f62f032b9a08
Miłosz Osiński <mylosz@yahoo.pl>
2014-04-04 23:30:46 +0200

    reorganizacja kodu ciąg dalszy

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,143 +1,126 @@
 class ImagesController < ApplicationController
 require 'net/ftp'
-  # GET /images
-  # GET /images.json
   def index
-   if current_user
-       if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
-    @images = Image.all
-    @news_kalendarz = News.all
-    @date = params[:month] ? Date.parse(params[:month].gsub('-', '/')) : Date.today
-    @zdjecia_stopka = Image.last(3)
-    @statystyki = Statistic.find_by_rok(Time.now.year)
+    if current_user
+      if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
+        @images = Image.all
+        @statystyki = Statistic.find_by_rok(Time.now.year)
 
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @images }
-    end
-	else
-  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
-  	end
-    else
-        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
-    end
-  end
-
-
-  # GET /images/1/edit
-  def edit
-  if current_user
-       if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
-    @news_kalendarz = News.all
-    @date = params[:month] ? Date.parse(params[:month].gsub('-', '/')) : Date.today
-    @image = Image.find(params[:id])
-    @zdjecia_stopka = Image.last(3)
-    @statystyki = Statistic.find_by_rok(Time.now.year)
-	else
-  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
-  	end
-    else
-        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
-    end
-  end
-
-  # POST /images
-  # POST /images.json
-  def create
-  if current_user
-       if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
-    		file = params[:file]
-		if News.find_all_by_id(params[:przydzial]).count != 0
-		@news = News.find(params[:przydzial])
-		@albums = Album.find_all_by_nr_newsa(@news.id)
-		else
-		@albums = Album.find_all_by_nr_newsa(params[:przydzial])
-		end
-		if file != nil && file.original_filename.end_with?('.jpg','.JPG','.png','.PNG','.gif','.GIF')
-		@zdjecia = Image.find_all_by_nazwa(file.original_filename).count
-		if (@zdjecia == 0)
-		  file.original_filename = file.original_filename.gsub(/\s+/, "")
-    		  ftp = Net::FTP.new('s3.masternet.pl')
-        	  ftp.passive = true
-    		  ftp.login(user = ENV['ftp_login'], passwd = ENV['ftp_haslo'])
-    		  ftp.storbinary("STOR " + file.original_filename, StringIO.new(file.read), Net::FTP::DEFAULT_BLOCKSIZE)
-    		  ftp.quit()
-		  @image = Image.new
-		  @image.nazwa = file.original_filename
-        	  @image.opis = params[:opis]
-		  @image.przydzial = params[:przydzial]
-		  if @albums.count == 0
-		  @album = Album.new
-		  @album.tytul = @news.tytul
-	          @album.nr_newsa = @news.id
-		  @album.save
-		  end
-
-    		  respond_to do |format|
-      		    if @image.save
-        		format.html { redirect_to @news, notice: 'Gratulacje! Dodano zdj&#281;cie do aktualno&#347;ci' }
-        		format.json { render json: @image, status: :created, location: @image }
-      		    else
-        		format.html { redirect_to @news, notice: 'Uwaga! Niepowodznie dodania zdjecia' }
-        		format.json { render json: @image.errors, status: :unprocessable_entity }
-      		    end
-    		  end
-		else
-		  redirect_to @news, :notice => 'Uwaga! Zdj&#281;cie o takiej nazwie ju&#380; jest w bazie!'
-		end
-		else
-		  redirect_to @news, :notice => 'Uwaga! Nie wybrano zdj&#281cia z komputera lub rozszerzenie jest nieprawid&#322owe!'
-		end
-	else
-  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
-  	end
-    else
-        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
-    end
-  end
-
-  # PUT /images/1
-  # PUT /images/1.json
-  def update
-if current_user
-       if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
-    @image = Image.find(params[:id])
-
-    respond_to do |format|
-      if @image.update_attributes(params[:image])
-        format.html { redirect_to "/galeria", notice: 'Gratulacje! Zdj&#281;cie zaktualizowano' }
-        format.json { head :no_content }
+        respond_to do |format|
+          format.html
+          format.json { render json: @images }
+        end
       else
-        format.html { render action: "edit" }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
+        redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
       end
-    end
-	else
-  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
-  	end
     else
-        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+      redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
     end
   end
 
-  # DELETE /images/1
-  # DELETE /images/1.json
-  def destroy
-  if current_user
-       if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
-    @image = Image.find(params[:id])
-    @image.destroy
-
-    respond_to do |format|
-      format.html { redirect_to images_url }
-      format.json { head :no_content }
-    end
-	else
-  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
-  	end
+  def edit
+    if current_user
+      if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
+        @image = Image.find(params[:id])
+        @statystyki = Statistic.find_by_rok(Time.now.year)
+      else
+        redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+      end
     else
-        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+      redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+
+  def create
+    if current_user
+      if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
+        file = params[:file]
+        if News.find_all_by_id(params[:przydzial]).count != 0
+          @news = News.find(params[:przydzial])
+          @albums = Album.find_all_by_nr_newsa(@news.id)
+        else
+          @albums = Album.find_all_by_nr_newsa(params[:przydzial])
+        end
+        if file != nil && file.original_filename.end_with?('.jpg','.JPG','.png','.PNG','.gif','.GIF')
+          @zdjecia = Image.find_all_by_nazwa(file.original_filename).count
+          if (@zdjecia == 0)
+            file.original_filename = file.original_filename.gsub(/\s+/, "")
+            ftp = Net::FTP.new('s3.masternet.pl')
+            ftp.passive = true
+            ftp.login(user = ENV['ftp_login'], passwd = ENV['ftp_haslo'])
+            ftp.storbinary("STOR " + file.original_filename, StringIO.new(file.read), Net::FTP::DEFAULT_BLOCKSIZE)
+            ftp.quit()
+            @image = Image.new
+            @image.nazwa = file.original_filename
+            @image.opis = params[:opis]
+            @image.przydzial = params[:przydzial]
+            if @albums.count == 0
+              @album = Album.new
+              @album.tytul = @news.tytul
+              @album.nr_newsa = @news.id
+              @album.save
+            end
+
+            respond_to do |format|
+              if @image.save
+                format.html { redirect_to @news, notice: 'Gratulacje! Dodano zdj&#281;cie do aktualno&#347;ci' }
+                format.json { render json: @image, status: :created, location: @image }
+              else
+                format.html { redirect_to @news, notice: 'Uwaga! Niepowodznie dodania zdjecia' }
+                format.json { render json: @image.errors, status: :unprocessable_entity }
+              end
+            end
+          else
+            redirect_to @news, :notice => 'Uwaga! Zdj&#281;cie o takiej nazwie ju&#380; jest w bazie!'
+          end
+        else
+          redirect_to @news, :notice => 'Uwaga! Nie wybrano zdj&#281cia z komputera lub rozszerzenie jest nieprawid&#322owe!'
+        end
+      else
+        redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+      end
+    else
+      redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+
+  def update
+    if current_user
+      if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
+        @image = Image.find(params[:id])
+
+        respond_to do |format|
+          if @image.update_attributes(params[:image])
+            format.html { redirect_to "/galeria", notice: 'Gratulacje! Zdj&#281;cie zaktualizowano' }
+            format.json { head :no_content }
+          else
+            format.html { render action: "edit" }
+            format.json { render json: @image.errors, status: :unprocessable_entity }
+          end
+        end
+      else
+        redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+      end
+    else
+      redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+    end
+  end
+
+  def destroy
+    if current_user
+      if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
+        @image = Image.find(params[:id])
+        @image.destroy
+
+        respond_to do |format|
+          format.html { redirect_to images_url }
+          format.json { head :no_content }
+        end
+      else
+        redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+      end
+    else
+      redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
     end
   end
 end
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,42 +1,42 @@
 class MembersController < ApplicationController
 
   def create
-   if current_user
-       if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
-    @member = Member.new(params[:member])
+    if current_user
+      if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
+        @member = Member.new(params[:member])
 
-    respond_to do |format|
-      if @member.save
-        format.html { redirect_to "/czlonkowie", notice: 'Gratulacje! Dodano nowego Cz&#322onka OSP.' }
-        format.json { render json: @member, status: :created, location: @member }
+        respond_to do |format|
+          if @member.save
+            format.html { redirect_to "/czlonkowie", notice: 'Gratulacje! Dodano nowego Cz&#322onka OSP.' }
+            format.json { render json: @member, status: :created, location: @member }
+          else
+            format.html { render action: "new" }
+            format.json { render json: @member.errors, status: :unprocessable_entity }
+          end
+        end
       else
-        format.html { render action: "new" }
-        format.json { render json: @member.errors, status: :unprocessable_entity }
+        redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
       end
-    end
-	else
-  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
-  	end
     else
-        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+      redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
     end
   end
 
   def destroy
-   if current_user
-       if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
-    @member = Member.find(params[:id])
-    @member.destroy
+    if current_user
+      if (current_user.username == 'Administrator'&&current_user.id==1)||(current_user.username == 'strazak'&&current_user.id==2)
+        @member = Member.find(params[:id])
+        @member.destroy
 
-    respond_to do |format|
-      format.html { redirect_to "/czlonkowie", notice: 'Informacja! Usuni&#281to Cz&#322onka OSP.' }
-      format.json { head :no_content }
-    end
-	else
-  	redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
-  	end
+        respond_to do |format|
+          format.html { redirect_to "/czlonkowie", notice: 'Informacja! Usuni&#281to Cz&#322onka OSP.' }
+          format.json { head :no_content }
+        end
+      else
+        redirect_to root_url, :notice => 'Uwaga! Nie masz uprawnie&#324;!'
+      end
     else
-        redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
+      redirect_to :login, :notice => 'Informacja! Zaloguj si&#281; aby obejrze&#263;!'
     end
   end
 end
