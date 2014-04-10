fa9f3e324e972dbb84c821c25c3b605ff6587db1
Miłosz Osiński <mylosz@yahoo.pl>
2014-04-10 22:05:43 +0200

    add comment count

--- config/application.rb
+++ config/application.rb
@@ -3,44 +3,24 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module Osp
   class Application < Rails::Application
     config.after_initialize do
-      Disqus::defaults[:account] = "osplesniewo"
+      Disqus::defaults[:account] = ENV['disqus_shortname']
       Disqus::defaults[:developer] = true
       Disqus::defaults[:container_id] = "disqus_thread"
       Disqus::defaults[:show_powered_by] = false
     end
 
     config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
-    # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
-
-    # Enable the asset pipeline
     config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.time_zone = 'Warsaw'
   end
